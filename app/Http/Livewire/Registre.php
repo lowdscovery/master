5294abc2237ea63cdc58282e0f005950fc573cca
stephan <stephangeorges980@gmail.com>
2024-03-08 12:52:51 +0100

    modifier login et registre2

---
+++ app/Http/Livewire/Registre.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class Registre extends Component
+{
+    public function render()
+    {
+        return view('passwords.registre');
+    }
+}
